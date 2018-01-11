5adc1b6cfc924f10785e13d488ecb2cd1689e4af
zoutong <514202874@qq.com>
2018-01-11 18:02:46 +0800

    Signed-off-by: zoutong <514202874@qq.com>

---
+++ Library/Book.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Book
+    {
+        private String _id;
+        private String _title;
+        private String _author;
+        private String _publisher;
+        private String _publishDate;
+        private String _language;
+        private String _pages;
+
+        public String ID
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+            }
+        }
+        public String Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        public String Author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+
+        public String Publisher
+        {
+            get { return _publisher; }
+            set { _publisher = value; }
+        }
+
+        public String PublishDate
+        {
+            get { return _publishDate; }
+            set { _publishDate = value; }
+        }
+
+        public String Language
+        {
+            get { return _language; }
+            set { _language = value; }
+        }
+
+        public String Pages
+        {
+            get { return _pages; }
+            set { _pages = value; }
+        }
+    }
+}
